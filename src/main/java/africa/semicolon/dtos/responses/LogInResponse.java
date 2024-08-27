a874bacea4015281694d457252c04e6847932f22
Precious <preciousonome002@gmail.com>
2024-08-27 09:39:29 +0100

    Login method

---
+++ src/main/java/africa/semicolon/dtos/responses/LogInResponse.java
@@ -0,0 +1,10 @@
+package africa.semicolon.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LogInResponse {
+    private String message;
+}
---
+++ src/main/java/africa/semicolon/exception/InvalidUserException.java
@@ -0,0 +1,6 @@
+package africa.semicolon.exception;
+
+public class InvalidUserException extends RuntimeException {
+    public InvalidUserException(String message) {
+    super(message);}
+}
